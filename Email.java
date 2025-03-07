6a78ce8c767d06d307f28962931d7900fe2cd7c3
iroha <78338683+little-jan@users.noreply.github.com>
2025-03-07 14:22:27 +0800

    Add files via upload

---
+++ Email.java
@@ -0,0 +1,13 @@
+package lab2;
+
+public class Email {
+    String subject;
+    String sender;
+    String message;
+
+    public Email(String sub, String send, String msg) {
+        subject = sub;
+        sender = send;
+        message = msg;
+    }
+}
